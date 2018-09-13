bebdaa9543897956887cd9748dcb3cfe317f54c5
Yates <my668618@ohio.edu>
2018-09-13 18:31:16 -0400

    ASSIGNMENT2

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    // Variable declaration and assignment
+    int a;
+    a = 445;
+
+    // Variable declaration and initialization
+    int b = 4;
+    int c = a;
+
+    //printf("Value of a = %d \nValue of b = %d \nValue of c = %d \n",a,b,c);
+
+    /*
+
+    */
+
+    printf("Value of a = %d\n", a);
+    printf("Value of b = %d\n", b);
+    printf("Value of c = %d\n", c);
+
+    //printf("Hello Darkness My Old Friend.\n");
+    return 0;
+}
